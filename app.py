8d475bcfd93091278c841aca7d098b4fc6737d55
John Traas <j.j.h.traas@gmail.com>
2020-06-26 13:15:57 +0000

    installed flask-pymongo and dnspython - added mongodb env variables

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+
+
+# Environment Variables
+app.config["MONGO_DBNAME"] = "rehearsal_room"
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def the_band_room():
     return 'Welcome to The Band Room'
 
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
