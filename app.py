9ccc0f6074d80a2dc3e1f9e2b91d333cede3ae0e
John Traas <j.j.h.traas@gmail.com>
2020-06-29 13:14:46 +0000

    added 'required' to input form areas - added check owner name and room key functionality

--- app.py
+++ app.py
@@ -17,16 +17,37 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/the_band_room')
+# Default landing page
 def the_band_room():
     return render_template("addbandroom.html",
                            )
 
 
+# This function adds a new band room
 @app.route('/add_room', methods=['POST'])
 def add_band_room():
-    new_room = mongo.db.band_rooms
-    new_room.insert_one(request.form.to_dict())
-    return redirect(url_for('browse_rooms'))
+    # take the information from the create room form
+    owner_name = request.form["owner_name"]
+    room_key = request.form["room_key"]
+    # error message
+    owner_key_not_available = "This user name and room key is not "\
+        "available. Please use a different event key."
+    # iterate over stored band rooms to check
+    # if the name and room key are available
+    check_user = mongo.db.band_rooms.count_documents((
+        {"owner_name": owner_name,
+         "room_key": room_key}))
+    if check_user > 0:
+        # if name and room key are not available
+        return render_template('roomkeyerror.html',
+                               owner_key_not_available=owner_key_not_available,
+                               owner_name=owner_name,
+                               room_key=room_key)
+    # if both name and key are available the room will be created
+    else:
+        new_room = mongo.db.band_rooms
+        new_room.insert_one(request.form.to_dict())
+        return redirect(url_for('browse_rooms'))
 
 
 @app.route('/band_room')
