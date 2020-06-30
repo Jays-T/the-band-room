583d88afcb2cabec5e8c2da714d75b5e43e7935c
John Traas <j.j.h.traas@gmail.com>
2020-06-30 14:55:04 +0000

    re-wrote my_room function using ObjectId

--- app.py
+++ app.py
@@ -53,23 +53,10 @@ def open_room():
     return render_template("openroom.html")
 
 
-@app.route('/my_room', methods=['POST'])
-def my_room():
-    band_name = request.form["band_name_open"]
-    room_key = request.form["room_key_open"]
-    check_rooms = mongo.db.band_rooms.count_documents((
-        {"room_key": room_key}))
-    if check_rooms > 0:
-        the_room = mongo.db.band_rooms.find_one(
-            {"band_name": request.form["band_name_open"],
-             "room_key": request.form["room_key_open"]})
-        return render_template("bandroom.html",
-                               the_room=the_room,
-                               band_name=band_name,
-                               room_key=room_key)
-    else:
-        flash('Oops seems like the Room Key was not right')
-        return redirect(url_for('open_room'))
+@app.route('/my_room/<room_id>')
+def my_room(room_id):
+    the_room = mongo.db.band_rooms.find_one({'_id': ObjectId(room_id)})
+    return render_template('bandroom.html', room=the_room)
 
 
 if __name__ == '__main__':
