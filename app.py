382d539482e3753ad3aa506858d257a3e1805b09
John Traas <j.j.h.traas@gmail.com>
2020-07-04 15:27:43 +0000

    added register.html and initial function to check for session

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+import bcrypt
 from flask import Flask, render_template, url_for, request, redirect, \
-     flash
+     flash, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists('env.py'):
@@ -19,6 +20,9 @@ mongo = PyMongo(app)
 @app.route('/the_band_room')
 # Default landing page
 def the_band_room():
+    if 'username' in session:
+        return 'You are logged in as ' + session[username]
+
     return render_template('addbandroom.html')
 
 
